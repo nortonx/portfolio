3334393a471cf293ed03e6b6f0c4d6f699dc9a21
Norton Almeida <norton.almeida@gmail.com>
2025-02-02 11:03:55 -0300

    Implemented project listing using useFetch composable.

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -18,6 +18,7 @@
 const links = [
   { name: 'Home', url: '/' },
   { name: 'Blog', url: '/blog' },
+  { name: 'Projects', url: '/projects' },
   { name: 'About', url: '/about' },
 ]
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto max-w-2xl">
     <NavMenu />
-    <main class="p-2">
+    <main class="p-2 mt-10">
       <slot />
     </main>
   </div>
