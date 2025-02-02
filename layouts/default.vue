f55cac2ab87fe4c4b46bfb2afa1238ac0c07c74c
Norton Almeida <norton.almeida@gmail.com>
2025-02-02 10:11:05 -0300

    Implemented SEO basic settings, will fix it later when setting up Google Analytics.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container mx-auto max-w-2xl">
     <NavMenu />
-    <main>
+    <main class="p-2">
       <slot />
     </main>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+useSeoMeta({
+  titleTemplate: "%s - Norton Almeida",
+})
 </script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,3 +3,14 @@
     This is the Blog index page
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Blog',
+  description: 'This is the blog index page',
+  image: 'https://nortonalmeida.dev/norton-almeida.jpg',
+  url: 'https://nortonalmeida.dev/blog',
+  type: 'website',
+  keywords: ['norton', 'almeida', 'norton almeida', 'nortonalmeida', 'norton almeida blog'],
+})
+</script>
